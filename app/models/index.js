3b0f0b3906e8b69ee6a014564be4c65398ac93af
Finn Kranzosch <49481876+FinnKr@users.noreply.github.com>
2020-05-19 11:35:40 +0200

    Fixed typo in models/index.js
    pool option "idle" got set to pool option "acquire" from the db.config.js

--- app/models/index.js
+++ app/models/index.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
         acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.acquire
+        idle: dbConfig.pool.idle
     }
 });
 
